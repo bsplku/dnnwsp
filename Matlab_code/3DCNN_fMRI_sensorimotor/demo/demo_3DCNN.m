8fc161a99405889387c7df9828cfedcc4f5c6276
Brain Signal Processing Lab <bsplku605@gmail.com>
2018-12-31 18:39:22 +0900

    Update demo_3DCNN.m

--- Matlab_code/3DCNN_fMRI_sensorimotor/demo/demo_3DCNN.m
+++ Matlab_code/3DCNN_fMRI_sensorimotor/demo/demo_3DCNN.m
@@ -6,7 +6,7 @@ net = CreateNet('3DCNN_config_sensorimotor.conf');
 
 % start training
 % net =  Train(FMRI,net); % train all 
-net =  Train(SEN,net,1000); % set number of volumes to use for training is 1000
+net =  Train(SEN,net,1000); % set the number of volumes to use for training is 1000
 
 % testing
 checkNetwork(net,Inf,SEN,1);
